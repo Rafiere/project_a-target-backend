97e0749a6332b6a4af3875faf934f60e44aef3e5
Rafiere <rafapgbr@gmail.com>
2023-09-13 08:54:15 -0300

    feat: add update time interval endpoint
    Signed-off-by: Rafiere <rafapgbr@gmail.com>

---
+++ src/main/java/com/atarget/atargetbackend/timer/controller/UpdateTimeIntervalByIdController.java
@@ -0,0 +1,28 @@
+package com.atarget.atargetbackend.timer.controller;
+
+import com.atarget.atargetbackend.timer.controller.request.UpdateTimeIntervalRequest;
+import com.atarget.atargetbackend.timer.controller.response.GetTimerIntervalResponse;
+import com.atarget.atargetbackend.timer.service.UpdateTimeIntervalByIdService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UpdateTimeIntervalByIdController {
+
+	private final UpdateTimeIntervalByIdService service;
+
+	@PatchMapping(path = "/timer/interval/{timeIntervalId}")
+	public ResponseEntity<GetTimerIntervalResponse> execute(@RequestBody UpdateTimeIntervalRequest request,
+	                                                        @PathVariable String timeIntervalId) {
+
+		GetTimerIntervalResponse result = service.execute(request, timeIntervalId);
+
+		return ResponseEntity.status(201)
+		                     .body(result);
+	}
+}
