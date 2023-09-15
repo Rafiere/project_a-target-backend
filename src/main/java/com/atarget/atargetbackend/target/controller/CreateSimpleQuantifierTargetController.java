dfba9a009e439c4b00e200992bb01e657c7b3577
Rafiere <rafapgbr@gmail.com>
2023-09-14 21:39:58 -0300

    feat: add creation of a simple quantifier target
    Signed-off-by: Rafiere <rafapgbr@gmail.com>

---
+++ src/main/java/com/atarget/atargetbackend/target/controller/CreateSimpleQuantifierTargetController.java
@@ -0,0 +1,25 @@
+package com.atarget.atargetbackend.target.controller;
+
+import com.atarget.atargetbackend.target.controller.request.CreateSimpleQuantifierTargetRequest;
+import com.atarget.atargetbackend.target.controller.response.CreateSimpleQuantifierTargetResponse;
+import com.atarget.atargetbackend.target.service.CreateSimpleQuantifierTargetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CreateSimpleQuantifierTargetController {
+
+	private final CreateSimpleQuantifierTargetService service;
+
+	@PostMapping(name = "/targets/simple-quantifier", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<CreateSimpleQuantifierTargetResponse> execute(@RequestBody CreateSimpleQuantifierTargetRequest request){
+
+		var response = service.execute(request);
+
+		return ResponseEntity.status(201).body(response);
+	}
+}
