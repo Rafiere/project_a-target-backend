5554fcef0d144d2de80759c0ec24a81314443c8f
Rafiere <rafapgbr@gmail.com>
2023-09-14 21:39:48 -0300

    feat: add creation of a simple target
    Signed-off-by: Rafiere <rafapgbr@gmail.com>

---
+++ src/main/java/com/atarget/atargetbackend/target/controller/CreateSimpleTargetController.java
@@ -0,0 +1,25 @@
+package com.atarget.atargetbackend.target.controller;
+
+import com.atarget.atargetbackend.target.controller.request.CreateSimpleTargetRequest;
+import com.atarget.atargetbackend.target.controller.response.CreateSimpleTargetResponse;
+import com.atarget.atargetbackend.target.service.CreateSimpleTargetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CreateSimpleTargetController {
+
+	private final CreateSimpleTargetService service;
+
+	@PostMapping(name = "/targets/simple", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<CreateSimpleTargetResponse> execute(@RequestBody CreateSimpleTargetRequest request){
+
+		var response = service.execute(request);
+
+		return ResponseEntity.status(201).body(response);
+	}
+}
