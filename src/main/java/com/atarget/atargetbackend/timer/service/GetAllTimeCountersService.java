347d214b3de72a38107759071f88456c087adee8
Rafiere <rafapgbr@gmail.com>
2023-09-14 21:40:49 -0300

    feat: add @Transactional
    Signed-off-by: Rafiere <rafapgbr@gmail.com>

--- src/main/java/com/atarget/atargetbackend/timer/service/GetAllTimeCountersService.java
+++ src/main/java/com/atarget/atargetbackend/timer/service/GetAllTimeCountersService.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class GetAllTimeCountersService {
 
 	private final TimeCounterRepository timeCounterRepository;
