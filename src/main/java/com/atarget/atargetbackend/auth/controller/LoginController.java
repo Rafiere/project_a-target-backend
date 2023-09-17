8156c7de16b3d90a6ef5b4c51f0d86332e1cfaef
Rafiere <rafapgbr@gmail.com>
2023-09-17 10:56:13 -0300

    feat: add login
    Signed-off-by: Rafiere <rafapgbr@gmail.com>

---
+++ src/main/java/com/atarget/atargetbackend/auth/controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.atarget.atargetbackend.auth.controller;
+
+import com.atarget.atargetbackend.auth.controller.request.LoginRequest;
+import com.atarget.atargetbackend.auth.controller.response.LoginResponse;
+import com.atarget.atargetbackend.auth.service.LoginService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LoginController {
+
+	private final LoginService loginService;
+
+	@PostMapping(path = "/login", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request){
+
+		var response = loginService.execute(request);
+
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+	}
+}
