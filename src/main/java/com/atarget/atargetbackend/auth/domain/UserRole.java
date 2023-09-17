94c5d43b1987d3c7e34c34623b604452f80f486d
Rafiere <rafapgbr@gmail.com>
2023-09-17 11:02:37 -0300

    feat: add user role enum
    Signed-off-by: Rafiere <rafapgbr@gmail.com>

---
+++ src/main/java/com/atarget/atargetbackend/auth/domain/UserRole.java
@@ -0,0 +1,19 @@
+package com.atarget.atargetbackend.auth.domain;
+
+public enum UserRole {
+
+	ADMIN("ADMIN"),
+	COMMON_USER("COMMON_USER");
+
+	private final String role;
+
+	UserRole(String role) {
+
+		this.role = role;
+	}
+
+	public String getRole() {
+
+		return role;
+	}
+}
