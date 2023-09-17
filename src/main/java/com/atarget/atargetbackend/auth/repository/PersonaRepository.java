2772eda7e35f22684a9e8a2ea4a258f918d49640
Rafiere <rafapgbr@gmail.com>
2023-09-17 11:03:27 -0300

    feat: add persona repository with it's queries
    Signed-off-by: Rafiere <rafapgbr@gmail.com>

---
+++ src/main/java/com/atarget/atargetbackend/auth/repository/PersonaRepository.java
@@ -0,0 +1,21 @@
+package com.atarget.atargetbackend.auth.repository;
+
+import com.atarget.atargetbackend.auth.domain.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public interface PersonaRepository extends JpaRepository<Persona, String> {
+
+	@Query("SELECT p.user " +
+	       "FROM Persona p " +
+	       "WHERE p.user.email = :email")
+	Optional<UserDetails> findByEmail(String email);
+
+	@Query("SELECT p " +
+	       "FROM Persona p " +
+	       "WHERE p.user.email = :email")
+	Optional<Persona> findPersonaByEmail(String email);
+}
