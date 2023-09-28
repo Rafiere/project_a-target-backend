8404e940a8b7284888813f36c0324cc8e5d45b59
Rafiere <rafapgbr@gmail.com>
2023-09-27 21:08:09 -0300

    feat: add controller routes
    Signed-off-by: Rafiere <rafapgbr@gmail.com>

---
+++ src/main/java/com/atarget/atargetbackend/shared/routes/RoutesGroups.java
@@ -0,0 +1,18 @@
+package com.atarget.atargetbackend.shared.routes;
+
+public enum RoutesGroups {
+
+	PERSONA("/personas"),
+	TARGET("/targets"),
+	AUTH("/auth");
+
+	private final String path;
+
+	RoutesGroups(String path){
+		this.path = path;
+	}
+
+	public String getPath() {
+		return path;
+	}
+}
