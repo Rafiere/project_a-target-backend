ef54148550fb77bcbc5d8ca214f0ca3e3f29c477
Rafiere <rafapgbr@gmail.com>
2023-09-30 19:15:54 -0300

    feat: add login returns access token
    Signed-off-by: Rafiere <rafapgbr@gmail.com>

--- src/main/java/com/atarget/atargetbackend/auth/controller/response/LoginResponse.java
+++ src/main/java/com/atarget/atargetbackend/auth/controller/response/LoginResponse.java
@@ -1,6 +1,10 @@
 package com.atarget.atargetbackend.auth.controller.response;
 
+import com.atarget.atargetbackend.shared.auth.enums.AuthTokenType;
 
-public record LoginResponse(AccessTokenResponse accessTokenResponse, RefreshTokenResponse refreshTokenResponse){
+public record LoginResponse(String accessToken, AuthTokenType tokenType, Long expirationInSeconds){
 
+	public static LoginResponse of(String accessToken, Long expirationInSeconds){
+		return new LoginResponse(accessToken, AuthTokenType.ACCESS_TOKEN, expirationInSeconds);
+	}
 }
