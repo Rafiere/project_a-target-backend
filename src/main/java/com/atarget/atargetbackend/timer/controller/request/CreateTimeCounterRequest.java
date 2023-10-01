5850b194a046fb12d757ccb10dd412cbf8c309f4
Rafiere <rafapgbr@gmail.com>
2023-10-01 11:04:12 -0300

    feat; add create time counter request validation
    Signed-off-by: Rafiere <rafapgbr@gmail.com>

--- src/main/java/com/atarget/atargetbackend/timer/controller/request/CreateTimeCounterRequest.java
+++ src/main/java/com/atarget/atargetbackend/timer/controller/request/CreateTimeCounterRequest.java
@@ -1,8 +1,24 @@
 package com.atarget.atargetbackend.timer.controller.request;
 
+import com.atarget.atargetbackend.shared.exception.custom.BusinessRuleException;
 import com.atarget.atargetbackend.timer.domain.enums.TimeCounterMethod;
 import com.atarget.atargetbackend.timer.domain.enums.TimeCounterType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record CreateTimeCounterRequest(String name, String description, TimeCounterType timeCounterType, TimeCounterMethod timeCounterMethod) {
+public record CreateTimeCounterRequest(@NotBlank String name, String description,
+                                       @NotNull TimeCounterType timeCounterType,
+                                       @NotNull TimeCounterMethod timeCounterMethod,
+                                       String targetAssignedToTimeCounterId) {
 
+	public void validate() {
+
+		if (timeCounterType.equals(TimeCounterType.ASSIGNED) &&
+		    (targetAssignedToTimeCounterId ==
+		     null ||
+		     targetAssignedToTimeCounterId.isBlank())) {
+
+			throw BusinessRuleException.of("A time counter of type ASSIGNED must have a target assigned to it.");
+		}
+	}
 }
