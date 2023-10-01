5204f51ec90bd67dd74680911ca4efff81362847
Rafiere <rafapgbr@gmail.com>
2023-10-01 11:02:40 -0300

    refactor! change final keyword
    Signed-off-by: Rafiere <rafapgbr@gmail.com>

--- src/main/java/com/atarget/atargetbackend/timer/controller/CreateTimeCounterController.java
+++ src/main/java/com/atarget/atargetbackend/timer/controller/CreateTimeCounterController.java
@@ -3,22 +3,28 @@ package com.atarget.atargetbackend.timer.controller;
 import com.atarget.atargetbackend.timer.controller.request.CreateTimeCounterRequest;
 import com.atarget.atargetbackend.timer.controller.response.CreateTimeCounterResponse;
 import com.atarget.atargetbackend.timer.service.CreateTimerCountService;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class CreateTimeCounterController {
 
 	private final CreateTimerCountService service;
 
-	@PostMapping(path = "/time-counters", consumes = "application/json", produces = "application/json")
-	public ResponseEntity<CreateTimeCounterResponse> execute(@RequestBody CreateTimeCounterRequest request){
+	@PostMapping(path = "/time-counters", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<CreateTimeCounterResponse> execute(@NotNull @RequestBody final CreateTimeCounterRequest request){
 
-		CreateTimeCounterResponse result = service.execute(request);
+		request.validate();
+
+		final CreateTimeCounterResponse result = service.execute(request);
 
 		return ResponseEntity.status(201).body(result);
 	}
